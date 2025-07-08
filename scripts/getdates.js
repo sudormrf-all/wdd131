1004ec081c9d98ecd6c928184233f68f564d9120
André Pina <andredspina@gmail.com>
2025-07-08 01:23:57 +0100

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+// Set current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+// Set last modified date
+document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
