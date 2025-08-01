ae0cfaaea226f62acbcc80b1b1a126fa8933086f
André Pina <andredspina@gmail.com>
2025-08-01 23:05:11 +0100

    W05 Assignment: Product Review Form

---
+++ scripts/review.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let count = Number(localStorage.getItem("reviewCount")) || 0;
+    count++;
+    localStorage.setItem("reviewCount", count);
+    document.getElementById("reviewCount").textContent = count;
+});
+  
